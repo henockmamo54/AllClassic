afd74a5dca52bb8a00ec985c3335e343372fafa6
Henock Mamo <henockmamo54@gmail.com>
2019-02-03 21:38:08 +0900

    refrences modified

--- AllClassicWeb/AllClassicWeb/App_Start/RouteConfig.cs
+++ AllClassicWeb/AllClassicWeb/App_Start/RouteConfig.cs
@@ -11,8 +11,7 @@ namespace AllClassicWeb
         public static void RegisterRoutes(RouteCollection routes)
         {
             var settings = new FriendlyUrlSettings();
-            //settings.AutoRedirectMode = RedirectMode.Permanent;
-            settings.AutoRedirectMode = RedirectMode.Off;
+            settings.AutoRedirectMode = RedirectMode.Permanent;
             routes.EnableFriendlyUrls(settings);
         }
     }
