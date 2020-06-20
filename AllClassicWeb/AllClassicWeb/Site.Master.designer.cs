5834576a95732b8322efe682af33a5f18cdda7a9
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-06-20 22:29:11 +0900

    about us page modified

--- AllClassicWeb/AllClassicWeb/Site.Master.designer.cs
+++ AllClassicWeb/AllClassicWeb/Site.Master.designer.cs
@@ -105,6 +105,15 @@ namespace AllClassicWeb
         protected global::System.Web.UI.HtmlControls.HtmlAnchor performancetab;
 
         /// <summary>
+        /// performancegrouptab control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor performancegrouptab;
+
+        /// <summary>
         /// auditiontab control.
         /// </summary>
         /// <remarks>
