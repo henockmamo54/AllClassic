bc8271951e34982981cc4fb0f2cc86580ffcfae7
DESKTOP-3BGEHE7\Henock <henockmamo54@gmail.com>
2020-08-12 00:09:38 +0900

    database model updated

--- AllClassicWeb/DataAccessP/Model1.Designer.cs
+++ AllClassicWeb/DataAccessP/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\workplace\C#\AllClassic_git\AllClassicWeb\DataAccessP\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\WorkPlace\github projects\allclassic 2020 asp\AllClassicWeb\DataAccessP\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
